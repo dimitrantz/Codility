add5d702a1aa166b8c73f2e9f6e1a0d5e6cde397
Dimitra Ntzioni <dntzioni@gmail.com>
2020-03-24 21:59:27 +0200

    upload new solution

---
+++ 7_StacksQueues/StoneWall.py
@@ -0,0 +1,24 @@
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+def solution(H):
+    # write your code in Python 3.6
+    if len(H) < 2: return len(H)
+    stack = []
+    count = 0
+    
+    for item in H:
+        
+        while len(stack) != 0 and stack[-1] > item:
+            stack.pop() 
+        
+        if len(stack) != 0 and stack[-1] == item:
+            continue
+        
+        stack.append(item)
+        #print(stack)
+        count += 1
+                    
+    return count
+                    
+            
