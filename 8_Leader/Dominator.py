a00bfc96a4a945b2c507ae461d8106dc370f3f5e
Dimitra Ntzioni <dntzioni@gmail.com>
2020-03-25 00:05:17 +0200

    upload new solutions

--- 8_Leader/Dominator.py
+++ 8_Leader/Dominator.py
@@ -1,23 +1,30 @@
-import random 
+# 100%
+from collections import Counter
 
 def solution(A):
+    
+    # write your code in Python 3.6
+    
     if len(A) == 0:
         return -1
 
-    num = int(len(A)/2 )
+    dominator, dominator_count = Counter(A).most_common()[0]
+
+    if dominator_count <= len(A) // 2:
+        return -1
+
+    return A.index(dominator)
+
+# not efficient
+def solution2(A):
+    
+    if len(A) == 0:
+        return -1
+    A.sort()
+    num = int(len(A)/2) 
     for i in range(int(len(A)/2)):
         if A[i] == A[i+num]:
-            return A[i]
+            return i
     return -1 
 
-
-if __name__ == "__main__":
-
-    fives = [5]*6
-    B = [1, 3, 2]
-    C = [4]
-    A = B + fives + C
-    #A = [2, 2, 2, 2, 2, 1, 4, 5, 6, 8]
-    #A = random. sample(range(-10, 30), 10)
-    #A = [1 , 1, 1, 1, 50]
-    print(solution(A))
+    
--- 8_Leader/EquiLeader.py
+++ 8_Leader/EquiLeader.py
@@ -1,49 +1,13 @@
-import random 
-
+# 100%
+from collections import Counter
 def solution(A):
-    if len(A) == 0:
-        return []
-    
-    indices = []
-    for i in range( len(A)):
-        if sum(A[:i]) == sum(A[i+1:]):
-            #return i
-            indices.append(i)
+    result = 0
+    dominator, dom_count  = Counter(A).most_common()[0]
+    left = 0
+    for idx, value in enumerate(A):
             
-    #in case you want return them all
-    if len(indices):
-        return indices
-    return -1
+        if value == dominator: left += 1
+        if left > (idx + 1)//2 and ((dom_count - left) > (len(A) - (idx + 1))//2):
+            result +=1
 
-def equ(A):
-    if len(A) == 0:
-        return []
-    indices = []
-    sum_r = sum(A)
-    sum_l = 0
-
-    for i in range(len(A)):
-        sum_r -= A[i]
-        if sum_r == sum_l:
-            #return i
-            indices.append(i)
-        sum_l +=A[i]
-
-    #in case you want return them all
-    if len(indices):
-        return indices
-    return -1
-    
-    
-
-
-
-if __name__ == "__main__":
-
-    
-    #A = [-1, 3, -4, 5, 1, -6, 2, 1]
-    #A = random. sample(range(-10, 30), 10)
-    A = [1 , -1, 2, -2, 3, -3, 4, -4, 1]
-    print (A)
-    print(solution(A))
-    print(equ(A))
+    return result
